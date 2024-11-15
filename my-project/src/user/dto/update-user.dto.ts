c251583670920958d8e1f494ac2750947532c5cf
Ekaterina Petukhova <ekaterina.petukhova@vaillant-group.com>
2024-11-15 08:26:58 +0100

    refactor: update updateUserSchemaDto

--- my-project/src/user/dto/update-user.dto.ts
+++ my-project/src/user/dto/update-user.dto.ts
@@ -1,6 +1,6 @@
 import { createUserDtoSchema } from './create-user.dto';
 import { z } from 'zod';
 
-export const updateUserDtoSchema = z.optional(createUserDtoSchema);
+export const updateUserDtoSchema = createUserDtoSchema.partial();
 
 export type UpdateUserDto = z.infer<typeof updateUserDtoSchema>;
