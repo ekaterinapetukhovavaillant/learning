107b8f87a3490020938a5914586849e344c5615a
Ekaterina Petukhova <ekaterina.petukhova@vaillant-group.com>
2024-11-17 15:13:51 +0100

    fix: add GetAllUsersService to providers

--- my-project/src/user/user.module.ts
+++ my-project/src/user/user.module.ts
@@ -4,10 +4,16 @@ import { UserController } from './user.controller';
 import { PrismaModule } from '../../src/prisma/prisma.module';
 import { CreateUserService } from './service/create-user.service';
 import { UpdateUserService } from './service/update-user.service';
+import { GetAllUsersService } from './service/get-all-users.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, CreateUserService, UpdateUserService],
+  providers: [
+    UserService,
+    GetAllUsersService,
+    CreateUserService,
+    UpdateUserService,
+  ],
 })
 export class UserModule {}
